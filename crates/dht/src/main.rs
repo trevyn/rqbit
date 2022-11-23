43b6db043287d2c0ab02a2512f458808b5bc5d17
trevyn <230691+trevyn@users.noreply.github.com>
2022-11-23 10:40:21 +0400

    remove pretty_env_logger dep

--- crates/dht/src/main.rs
+++ crates/dht/src/main.rs
@@ -7,7 +7,7 @@ use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    pretty_env_logger::init();
+    // pretty_env_logger::init();
 
     let info_hash = Id20::from_str("64a980abe6e448226bb930ba061592e44c3781a1").unwrap();
     let dht = Dht::new().await.context("error initializing DHT")?;
